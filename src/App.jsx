518b2e3ed5a25be622d3939a0212b9f33e83567b
VietAnh3004 <anhanhdeptrai304@gmail.com>
2025-11-04 18:12:44 +0700

    fix Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
 import React from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/SideBar/SideBar';
-import BoardsPage from './pages/BoardsPage/BoardsPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.jsx';
 import AuthLayout from './layouts/AuthLayout.jsx';
 import ManagementTable from './pages/ManagementTable/ManagementTable';
-import './App.css';
+import BoardsPage from './pages/BoardsPage/BoardsPage';
 import Login from './pages/Auth/Login.jsx';
 import Register from './pages/Auth/Register.jsx';
 import ForgotPassword from './pages/Auth/ForgotPassword.jsx';
 
-
 const App = () => {
-  const location = useLocation();
-
-  const showSidebar = location.pathname.startsWith('/main');
-
   return (
-    <div className="app">
-      <Navbar />
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Route>
 
-      {showSidebar ?
-        ( // Bố cục có sidebar 
-          <div className="main-layout">
-            <Sidebar />
-            <div className="page-content">
-              <Routes>
-                <Route path="/main/boards" element={<BoardsPage />} />
-              </Routes>
-            </div>
-          </div>
-        ) : (
-          <div className="fullpage-content">
-            <Routes>
-              <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route element={<MainLayout />}>
+        <Route path="/main/boards" element={<BoardsPage />} />
+        <Route path="/boards/:title" element={<ManagementTable />} />
+      </Route>
 
-              {/* Auth Pages */}
-              <Route element={<AuthLayout />}>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-              </Route>
-
-              <Route path="/boards/:title" element={<ManagementTable />} />
-            </Routes>
-          </div>
-        )}
-    </div>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      
+    </Routes>
   );
 }
 
-export default App
+export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import Sidebar from '../components/SideBar/SideBar';
+import './MainLayout.css';
 
 const MainLayout = () => {
   return (
-    <div className="main-app-layout">
+    <div className="app">
       <Navbar />
-      <main className="content">
-        <Outlet /> 
-      </main>
+      <div className="main-layout">
+        <Sidebar />
+        <div className="page-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
