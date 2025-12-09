d40238603e1d1b7e69b4ec873a829c30c187bba7
VietAnh3004 <anhanhdeptrai304@gmail.com>
2025-12-09 17:38:41 +0700

    change API to local

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://workflow-0euv.onrender.com'; 
+const API_BASE_URL = 'http://localhost:5174'; 
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
