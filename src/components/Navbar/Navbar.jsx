e836570a218295dc1afe5b445b9e40afc0bc435c
quocanhtutin <quocanhtutin@gmail.com>
2026-01-06 00:38:38 +0700

    fix mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,67 +5,74 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axiosClient from '../../utils/axiosConfig';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const navigate = useNavigate();
+    const [showMenu, setShowMenu] = useState(false);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isBoardPage = location.pathname.startsWith('/board/');
 
-  const handleLogout = async () => {
-    try {
-      const refreshToken = localStorage.getItem('refreshToken');
-      const provider = localStorage.getItem('provider');
+    const handleLogout = async () => {
+        try {
+            const refreshToken = localStorage.getItem('refreshToken');
+            const provider = localStorage.getItem('provider');
 
-      await axiosClient.post('/Auth/Logout', {
-        provider: provider,
-        refreshToken: refreshToken
-      });
-    } catch (error) {
-      console.error("Lỗi logout trên server:", error);
-    } finally {
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('refreshToken');
-      localStorage.removeItem('provider');
-      localStorage.removeItem('returnUrl');
+            await axiosClient.post('/Auth/Logout', {
+                provider: provider,
+                refreshToken: refreshToken
+            });
+        } catch (error) {
+            console.error("Lỗi logout trên server:", error);
+        } finally {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            localStorage.removeItem('provider');
+            localStorage.removeItem('returnUrl');
 
-      navigate('/login');
-    }
-  };
+            navigate('/login');
+        }
+    };
 
-  const handleNavigate = (path) => {
-    navigate(path);
-    setShowMenu(false);
-  };
+    const handleNavigate = (path) => {
+        navigate(path);
+        setShowMenu(false);
+    };
 
-  return (
-    <div className="navbar">
-      <div className="navbar-left">
-  <button className="icon-btn" onClick={() => navigate('/home')}>
-    <Home size={18} />
-  </button>
-  <input type="text" placeholder="Tìm kiếm" className="search-input" />
-</div>
-      <div className="navbar-right">
-        <button className="icon-btn"><Plus /></button>
-        <button className="icon-btn"><Bell /></button>
-        <div className="menu-wrapper">
-          <button className="icon-btn" onClick={() => setShowMenu(!showMenu)}><User /></button>
-          {showMenu && (
-            <div className="navbar-dropdown-menu">
-              <ul>
-                <li onClick={() => handleNavigate('/main/settings')}>Tài khoản</li>
-                <li>Cài đặt</li>
-                <li>Chủ đề</li>
-                <li>Trợ giúp</li>
-                <li>
-                  <button onClick={handleLogout} className="logout-btn">
-                    Đăng xuất
-                  </button>
-                </li>
-              </ul>
+    return (
+        <div className="navbar">
+            {isBoardPage ? (
+                <div className="navbar-left">
+                    <button className="icon-btn" onClick={() => navigate('/home')}>
+                        <Home />
+                    </button>
+                    <input type="text" placeholder="Tìm kiếm" className="search-input" />
+                </div>
+            ) : (
+                <div className="navbar-left">
+                    <h2 className='navbar-title'>WORKHUB360</h2>
+                </div>)}
+            <div className="navbar-right">
+                <button className="icon-btn"><Plus /></button>
+                <button className="icon-btn"><Bell /></button>
+                <div className="menu-wrapper">
+                    <button className="icon-btn" onClick={() => setShowMenu(!showMenu)}><User /></button>
+                    {showMenu && (
+                        <div className="navbar-dropdown-menu">
+                            <ul>
+                                <li onClick={() => handleNavigate('/main/settings')}>Tài khoản</li>
+                                <li>Cài đặt</li>
+                                <li>Chủ đề</li>
+                                <li>Trợ giúp</li>
+                                <li>
+                                    <button onClick={handleLogout} className="logout-btn">
+                                        Đăng xuất
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+        </div >
+    );
 };
 
 export default Navbar;
